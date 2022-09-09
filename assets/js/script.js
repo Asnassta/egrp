34345738d141bc8898f82afdbf69378f9b3463cf
Asnassta <79850172+Asnassta@users.noreply.github.com>
2022-09-09 09:39:54 +0300

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,147 @@
+$(document).ready(function() {
+
+  /*======Nav (dropdown)=============*/
+  $(".burger").on("click", function() {
+    $(this).toggleClass('active');
+    $(".nav").slideToggle(333);
+  });
+  /*==========/nav (dropdown)=========*/
+
+  /*======Card-roll (dropdown)=============*/
+  $(".card-roll .title-small").on("click", function() {
+    $(this).toggleClass('active');
+    $(this).next().slideToggle(333);
+  });
+  /*==========/card-roll (dropdown)=========*/
+
+  /*======Favorite=============*/
+  $(".favorite").on("click", function(event) {
+    event.preventDefault();
+    $(this).toggleClass('active');
+  });
+  /*==========/favorite=========*/
+
+  /*=================Sliders==========================*/
+  /*===========Slider-product============*/
+  $('.buy .product-new .product__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+     {
+       breakpoint: 1230,
+        settings: {
+            infinite: true,
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+        }
+      },
+    ]
+  });
+
+  $('.buy .product-secondary .product__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+     {
+       breakpoint: 1230,
+        settings: {
+            infinite: true,
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+        }
+      },
+    ]
+  });
+
+  $('.rent .product-new .product__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+     {
+       breakpoint: 1230,
+        settings: {
+            infinite: true,
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+        }
+      },
+    ]
+  });
+
+  $('.rent .product-secondary .product__inner').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    arrows: false,
+    responsive: [
+     {
+       breakpoint: 1230,
+        settings: {
+            infinite: true,
+            variableWidth: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            dots: true,
+        }
+      },
+    ]
+  });
+  /*===========/slider-product============*/
+
+  /*=========/sliders==========*/
+
+});
+
+
+/*=========Scrollpage (header show)=====*/
+$(document).on('wheel', function(e){
+
+  if (e.originalEvent.wheelDelta >= 0) {
+
+    $(".header").addClass('show');
+     $(".system-bar").addClass('hide');
+
+
+  } else {
+
+      $(".header.show .nav").slideUp();
+      $(".burger").removeClass('active');
+      $(".header").removeClass('show');
+      $(".system-bar").removeClass('hide');
+  }
+});
+
+var last;
+
+$(document).bind('touchmove', function(e){
+
+   var current = e.originalEvent.touches[0].clientY;
+
+   if(current > last){
+
+     $(".header").addClass('show');
+     $(".system-bar").addClass('hide');
+
+   } else if(current < last){
+
+      $(".header").removeClass('show');
+      $(".system-bar").removeClass('hide');
+
+   }
+
+   last = current;
+
+});
+/*=========/scrollpage (header show)=====*/
