80d1c82d54d7a0ca97dfc4ddb91032a2ed498a81
Asnassta <79850172+Asnassta@users.noreply.github.com>
2022-10-24 13:11:50 +0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -265,10 +265,6 @@ $(document).ready(function() {
     $(".chat-list__item").not(this).removeClass('active');
     $('.chat-screen').hide();
     $('.chat-screen.'+$(this).attr('data-chat')).fadeIn(222);
-    if($(window).width() <= 765)
-      {
-        $('body').addClass("hidden");
-      }
   });
   /*==========/chat-list=========*/
 
@@ -283,7 +279,6 @@ $(document).ready(function() {
 
   $(".chat-back.mobile").on("click", function() {
     $(".chat-screen").fadeOut();
-    $('body').removeClass("hidden");
   });
   /*==========/chat-screen-person=========*/
 
